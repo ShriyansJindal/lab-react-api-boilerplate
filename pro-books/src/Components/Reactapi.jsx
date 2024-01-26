9a55e03f2e8bcdb9e00617201ed6944a4698bf92
shriyans jindal <shriyansjindal40517@gmail.com>
2024-01-26 12:38:06 +0530

    done

--- pro-books/src/Components/Reactapi.jsx
+++ pro-books/src/Components/Reactapi.jsx
@@ -3,7 +3,7 @@ import axios from 'axios'
 import "./Reactapi.css"
 
 const Reactapi = () => {
-    let [data,setData]= useState()
+    let [data,setData]= useState([])
 
     useEffect(()=>{
         axios.get("https://reactnd-books-api.udacity.com/books",{ headers: { 'Authorization': 'whatever-you-want' }}).then(res=>{
